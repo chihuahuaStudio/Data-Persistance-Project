9803dbddc916342f4a89e41db2af53bdb601b755
chihuahuaStudio <82788392+chihuahuaStudio@users.noreply.github.com>
2021-06-13 21:06:45 -0400

    Initial commit
    starter breakout game imported

---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class QuitButton : MonoBehaviour
+{
+    public void EXit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+    Application.Quit();
+#endif
+
+    }
+   
+}
